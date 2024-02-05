07f25581ba4701a707ea760963c67d7455c0816d
Babu Chandranna <babubhare@gmail.com>
2024-02-05 14:31:57 +0000

    interrupt

---
+++ 1/boot_interrupt.asm
@@ -0,0 +1,52 @@
+ORG 0
+BITS 16
+
+jmp 0x7c0: start
+
+handle_interrupt: 
+    mov ah, 0eh
+    mov al, "I"
+    mov bx, 0x00
+    int 0x10
+    iret
+
+
+start:
+    cli
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
+
+    mov word[ss:0x00], handle_interrupt
+    mov word[ss:0x02], 0x7c0
+    int 0
+
+    mov si, message
+    call print
+    jmp $
+
+print:
+    mov bx, 0
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'Hello World!', 0
+
+times 510-($ - $$) db 0
+dw 0xAA55
