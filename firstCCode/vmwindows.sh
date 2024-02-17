d8ee049a820ee616603378c6f677c68c4bd52678
babuilandinfra <babuilandinfra@gmail.com>
2024-02-17 23:47:42 +0530

    windows

--- firstCCode/vmwindows.sh
+++ firstCCode/vmwindows.sh
@@ -1,1 +1,17 @@
-test
+qemu-system-x86_64 -hda ./bin/os.bin
+
+target remote | qemu-system-x86_64 -hda ./bin/os.bin -S -gdb stdio -nographic 
+
+target remote | qemu-system-x86_64 -hda ./bin/os.bin -S -gdb tcp:127.0.0.1:1234 stdio -s
+
+qemu-system-x86_64.exe -hda ./bin/os.bin -S
+
+
+For Windows system
+
+In one terminal of mingw:
+qemu-system-x86_64 -hda ./bin/os.bin -S -gdb tcp:127.0.0.1:1234
+
+In another terminal of mingw:
+gdb
+target remote tcp:127.0.0.1:1234
