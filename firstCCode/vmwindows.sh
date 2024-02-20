8f41cac72060f0dc0c1ade317eb7872259e52405
babuilandinfra <babuilandinfra@gmail.com>
2024-02-20 23:28:42 +0530

    commands

--- firstCCode/vmwindows.sh
+++ firstCCode/vmwindows.sh
@@ -12,6 +12,18 @@ For Windows system
 In one terminal of mingw:
 qemu-system-x86_64 -hda ./bin/os.bin -S -gdb tcp:127.0.0.1:1234
 
+**************************************************************************************
 In another terminal of mingw:
 gdb
 target remote tcp:127.0.0.1:1234
+add-symbol-file ./build/kernelfull.o 0x0100000
+https://ftp.gnu.org/old-gnu/Manuals/gdb/html_node/gdb_toc.html#TOC56
+
+gdb:
+info registers
+frame
+c
+n
+cntrl c
+disas 0x0100000, 0x0100fff
+**************************************************************************************
