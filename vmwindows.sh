f62e976813c228bdf16e03a7102dc34dac3091d4
Babu Chandranna <babubhare@gmail.com>
2024-03-04 16:55:36 +0530

    cross compiler

--- vmwindows.sh
+++ vmwindows.sh
@@ -33,3 +33,25 @@ cntrl c
 disas 0x0100000, 0x0100fff
 layout asm
 **************************************************************************************
+
+**************************************************************************************
+cross compiler codebase
+
+https://ftp.gnu.org/gnu/gdb/
+https://ftp.gnu.org/gnu/binutils/
+
+curl "https://ftp.gnu.org/gnu/binutils/binutils-2.42.tar.gz" --output binutils-2.42.tar.gz
+curl "https://ftp.gnu.org/gnu/gdb/gdb-14.2.tar.gz" --output gdb-14.2.tar.gz
+
+git clone git://gcc.gnu.org/git/gcc.git
+
+Extract
+tar -xvzf [archive name]
+tar -xvzf binutils-2.42.tar.gz
+tar -xvzf gdb-14.2.tar.gz
+
+export PREFIX="$HOME/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+
+**************************************************************************************
