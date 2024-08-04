bfadc8f77dd896588d15b9b5a17788e97c22f72a
Babu Chandranna <babubhare@gmail.com>
2024-08-04 17:39:48 +0000

    qemu strt

--- videorecording/installation/ubuntu-qemu-nasm.sh
+++ videorecording/installation/ubuntu-qemu-nasm.sh
@@ -4,3 +4,4 @@ sudo apt-get install nano --assume-yes
 sudo apt-get install gdb --assume-yes
 sudo apt-get install nasm --assume-yes 
 sudo apt-get install qemu-system-x86 --assume-yes
+sudo apt install qemu-system-gui --assume-yes
