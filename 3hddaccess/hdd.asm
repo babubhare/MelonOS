8f1a94c740d7d0d5b2365428c0816856f59e87f6
Babu Chandranna <babubhare@gmail.com>
2024-08-18 16:23:59 +0000

    hdd

--- 3hddaccess/hdd.asm
+++ 3hddaccess/hdd.asm
@@ -1,10 +1,48 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+    times 33 db 0
+
 start:
-    mov si, message
+    jmp 0x7c0: step2
+
+
+step2:
+    cli
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
+
+    ; REad from HDD and load to the buffer address
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer
+    int 0x13
+
+    jc error
+
+    mov si, buffer
+    call print
+
+    jmp $
+
+error:
+    mov si, error_message
     call print
     jmp $
+
 
 print:
     mov bx, 0
@@ -23,6 +61,8 @@ print_char:
     ret
 
 message: db 'Hello World!', 0
-
+error_message: db 'Error Message', 0
 times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer:
