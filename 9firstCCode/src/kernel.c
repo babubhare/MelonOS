ebb19b0c9d8ac3c596744b85df19eba88204dc12
Babu Chandranna <babubhare@gmail.com>
2024-08-30 14:58:37 +0000

    new files sh

--- 9firstCCode/src/kernel.c
+++ 9firstCCode/src/kernel.c
@@ -2,5 +2,11 @@
 
 void kernel_main()
 {
+    int intValue1 = 100;
+    int intValue2 = 200;
 
+    if(intValue1 == intValue2)
+    {
+        
+    }
 }
---
+++ ctryoutcode/gdbtryint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int value1 = 10;
+    int value2 = 20;
+
+
+    printf("testing integer\n");
+
+    if(value1 == value2)
+    {
+
+    }
+
+
+    return 0;
+}
