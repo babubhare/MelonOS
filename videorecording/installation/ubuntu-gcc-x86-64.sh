cc3cf6dafd1d8ef042caace212ac74af5099a2ad
= <=>
2024-09-16 01:35:18 -0400

    new files sh

--- videorecording/installation/ubuntu-gcc-x86-64.sh
+++ videorecording/installation/ubuntu-gcc-x86-64.sh
@@ -1,30 +1,6 @@
 #!/bin/sh
-sudo apt-get update --assume-yes 
-#sudo apt-get upgrade --assume-yes
 
-sudo apt-get install build-essential --assume-yes
-sudo apt-get install git --assume-yes
-
-cd $HOME
-mkdir src
-cd src
-
-curl "https://ftp.gnu.org/gnu/gdb/gdb-14.2.tar.gz" --output gdb-14.2.tar.gz
-tar -xvzf gdb-14.2.tar.gz
-curl "https://ftp.gnu.org/gnu/binutils/binutils-2.42.tar.gz" --output binutils-2.42.tar.gz
-tar -xvzf binutils-2.42.tar.gz
-curl "https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.gz" --output gcc-13.2.0.tar.gz
-tar -xvzf gcc-13.2.0.tar.gz
-
-sudo apt-get install bison --assume-yes
-sudo apt-get install flex --assume-yes
-sudo apt-get install libgmp3-dev --assume-yes
-sudo apt-get install libmpc-dev --assume-yes
-sudo apt-get install libmpfr-dev --assume-yes
-sudo apt-get install texinfo --assume-yes
-sudo apt-get install libisl-dev --assume-yes
-
-export PREFIX="$HOME/opt/cross"
+export PREFIX="$HOME/opt/cross86"
 #export TARGET=i686-elf
 export TARGET=x86_64-elf
 export PATH="$PREFIX/bin:$PATH"
