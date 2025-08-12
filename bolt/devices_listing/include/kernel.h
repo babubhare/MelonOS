1f35f15c0bf18e2e2f55ef1def25624ec6ce279f
root <root@v-os-Ubuntu24.04noble6.8.0-45-generic>
2025-08-12 16:29:34 +0000

    new files sh

--- bolt/devices_listing/include/kernel.h
+++ bolt/devices_listing/include/kernel.h
@@ -7,7 +7,7 @@
 // Basic type definitions
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
+//typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 
 // VGA text mode constants
